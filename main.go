f6ca3589d2e6be948411b8fa43fcce47abf70f92
kesarsauce <50715271+kesarsauce@users.noreply.github.com>
2022-10-05 23:04:35 +0530

    Update main.go

--- main.go
+++ main.go
@@ -17,8 +17,9 @@ type album struct {
 // albums slice to seed record album data.
 var albums = []album{
     {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+    {ID: "2", Title: "Queen", Artist: "Gerry Mulligan", Price: 17.99},
     {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+    {ID: "4", Title: "Hocus Pocus", Artist: "Focus", Price: 49.99},
 }
 
 func main() {
